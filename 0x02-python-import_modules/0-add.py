5630d62c8395057ba415ceba5afb8636e594cffe
batsieba <batisebatekle@gmail.com>
2024-01-05 01:05:07 +0400

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add_0
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a,b)}\n")
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
